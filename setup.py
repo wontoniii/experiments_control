aaa6d336c49865e4daf0efed5221ee1b1fee0fba
Francesco Bronzino <bronzino@winlab.rutgers.edu>
2017-09-21 13:16:56 +0200

    First release version

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='experiments_control',
+      version='0.1',
+      description='Simple framework to control experiemnts in a local network',
+      url='https://github.com/wontoniii/experiments_control',
+      author='Francesco Bronzino',
+      author_email='wontoniii@gmail.com',
+      license='MIT',
+      zip_safe=False)
