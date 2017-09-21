bff1a01c4da26e4443ac048e195ca638a5a89f25
Francesco Bronzino <bronzino@winlab.rutgers.edu>
2017-09-21 14:29:56 +0200

    Refactored name

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='experiments_control',
+setup(name='expscontrol',
       version='0.1',
       description='Simple framework to control experiments in a local network',
-      url='https://github.com/wontoniii/experiments_control',
+      url='https://github.com/wontoniii/expscontrol',
       author='Francesco Bronzino',
       author_email='wontoniii@gmail.com',
       license='MIT',
-      packages=['experiments_control'],
+      packages=['expscontrol'],
       zip_safe=False)
