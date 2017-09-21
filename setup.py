4c600526c69e0f94d3b5bce6426212e6bef7ae37
Francesco Bronzino <bronzino@winlab.rutgers.edu>
2017-09-21 14:11:27 +0200

    Small fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='experiments_control',
       version='0.1',
-      description='Simple framework to control experiemnts in a local network',
+      description='Simple framework to control experiments in a local network',
       url='https://github.com/wontoniii/experiments_control',
       author='Francesco Bronzino',
       author_email='wontoniii@gmail.com',
