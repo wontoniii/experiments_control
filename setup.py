d7fc76ec3a91043337948c16472525f78c72b678
Francesco Bronzino <bronzino@winlab.rutgers.edu>
2018-01-25 15:59:30 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='expscontrol',
-      version='0.1',
+      version='0.1.1',
       description='Simple framework to control experiments in a local network',
       url='https://github.com/wontoniii/expscontrol',
       author='Francesco Bronzino',
